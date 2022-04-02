1a85ba6fee0ac37d16ee559af2b0b0c6e0d61c96
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2022-04-02 18:30:45 +0100

    first concept.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+const infos = [
+  {txt: "MK", ico:"perm_identity"},
+  {txt:"609081641", ico:"call"},
+  {txt:"kozdrojmateusz86@gmail.com", ico:"mail"},
+  {txt:"ul. ks. Fr. Koleboka", ico:"location_on"},
+  {txt:"Usługi ślusarsko-spawalnicze", ico:"arrow_forward"},
+  {txt:"spawanie MIG, MAT, TIG", ico:"arrow_forward"},
+  {txt:"cięcie rur", ico:"arrow_forward"},
+  {txt:"szlifowanie", ico:"arrow_forward"},
+  {txt:"wiercenie", ico:"arrow_forward"},
+]
+const Smasher = ({ food }) => {
+  return (
+    <div className="wide-box">
+      <i className="large material-icons oranger">{food.ico}</i>
+      <p>{food.txt}</p>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {    infos.map(el=><Smasher food={el} /> )    }
     </div>
   );
 }
