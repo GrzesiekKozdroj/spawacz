ed37779119609a6a7d1abf22044d0f7017b41ee2
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2022-04-05 13:22:07 +0100

    page complete.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 const infos = [
-  {txt: "MK", ico:"perm_identity"},
-  {txt:"609081641", ico:"call"},
-  {txt:"kozdrojmateusz86@gmail.com", ico:"mail"},
+  {txt:"Mateusz Kozdrój", ico:"perm_identity"},
+  {txt:"609081641", ico:"call", kalbak:(num)=>window.open(`tel:${num}`, '_self')},
+  {txt:"kozdrojmateusz86@gmail.com", ico:"mail", linker:`"mailto: kozdrojmateusz86@gmail.com"`},
   {txt:"ul. ks. Fr. Koleboka", ico:"location_on"},
   {txt:"Usługi ślusarsko-spawalnicze", ico:"arrow_forward"},
   {txt:"spawanie MIG, MAT, TIG", ico:"arrow_forward"},
@@ -13,15 +12,28 @@ const infos = [
 ]
 const Smasher = ({ food }) => {
   return (
-    <div className="wide-box">
+    <div className="wide-box" onClick={ food.kalbak ? ()=>food.kalbak(food.txt) : () => null }>
       <i className="large material-icons oranger">{food.ico}</i>
-      <p>{food.txt}</p>
+      {
+        food.linker ? 
+          <a href={food.linker} >
+            <p>{food.txt}</p>
+          </a> : <p>{food.txt}</p>
+      }
     </div>
   )
 }
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <div className="header-center-box">
+          <div className="header-dummy">
+            MK
+            <div className="white-line">KOZDRÓJ</div>
+          </div>
+        </div>
+      </div>
       {    infos.map(el=><Smasher food={el} /> )    }
     </div>
   );
